0e6612fb5b88ca5ec7c0434892176ee6d1291077
immkavin-ranks <immkavin.ofcl@gmail.com>
2023-10-06 19:02:07 +0530

    Remove older functions

--- main.py
+++ main.py
@@ -49,30 +49,3 @@ def caesar(mode, input_text, shift_amount):
 
 
 caesar(mode=direction, input_text=text, shift_amount=shift)
-
-# def encrypt(plain_text, shift_amount):
-# cipher_text = ""
-# for letter in plain_text:
-#     letter_index = alphabet.index(letter)
-#     shifted_index = letter_index + shift_amount
-#     while not shifted_index < 26:
-#         shifted_index -= 26
-#     cipher_text += alphabet[shifted_index]
-# print(f"The encoded text is {cipher_text}")
-
-
-# def decrypt(cipher_text, shift_amount):
-#     plain_text =""
-#     for letter in cipher_text:
-#         letter_index = alphabet.index(letter)
-#         shifted_index = letter_index - shift_amount
-#         while not shifted_index >= 0:
-#             shifted_index += 26
-#         plain_text += alphabet[shifted_index]
-#     print(f"The decoded text is {plain_text}")
-
-
-# if direction == 'encode':
-#     encrypt(plain_text=text, shift_amount=shift)
-# elif direction == 'decode':
-#     decrypt(cipher_text=text, shift_amount=shift)
