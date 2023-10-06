36b2b750fc0dd9506bc4bda3aaebd13e41e602fd
immkavin-ranks <immkavin.ofcl@gmail.com>
2023-10-06 19:44:47 +0530

    Create art and alpha

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
-alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
+from alpha import alphabet
+from art import logo
 
 
 def caesar(mode, input_text, shift_amount):
